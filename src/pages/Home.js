f9b12b533aac81f849f8120a19090e709d650ebe
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-07-19 16:23:19 +0330

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+
+
+import Branches from './home/Branches';
+import Menu from './home/Menu';
+
+
+
+const Home =()=>{
+return(
+    <>
+      <div><Menu/></div>
+      <div><Menu/></div>
+     <div><Branches/></div> 
+    
+    </>
+  
+   
+
+)
+   
+}
+export default Home;
