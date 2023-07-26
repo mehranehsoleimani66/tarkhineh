c7f3e6e2350ff49863f994435b82625119807e41
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-07-26 12:59:02 +0330

    create slider component

---
+++ src/component/slider/Slider.js
@@ -0,0 +1,31 @@
+import {Button, Carousel} from 'antd';
+
+const Slider = () => {
+    
+       
+    return ( 
+        
+          <Carousel arrow prevArrow={<div className='text-white '>kskskks</div>} nextArrow={<div>sksksks</div>} >
+            <div className='relative' >
+              <img style={{width:'100%'}} src='./images/slider.png'/>
+              <h4 style={{fontSize:'20px', position:'absolute', top:'45%', left:'39%',color:'white',margin:'auto'}}>تجربه غذای سالم و گیاهی به سبک ترخینه</h4>
+               <Button style={{ position:'absolute', bottom:'25%', left:'46%',backgroundColor:'green',margin:'auto',color:'white'}}>سفارش آنلاین غذا</Button>
+           <div style={{}}></div>
+            </div>
+            <div className='relative'>
+            <img style={{width:'100%'}} src='./images/slider2.png'/>
+            <h4 style={{fontSize:'25px', position:'absolute', top:'45%', left:'43%',color:'white',margin:'auto'}}>!طعم بی نظیر طبیعت</h4>
+               <Button style={{ position:'absolute', bottom:'25%', left:'45%',backgroundColor:'green',margin:'auto',color:'white'}}>سفارش آنلاین غذا</Button>
+            </div>
+            <div className='relative'>
+            <img style={{width:'100%'}} src='./images/slider3.png'/>
+          <h4 style={{fontSize:'25px', position:'absolute', top:'45%', left:'40%',color:'white',margin:'auto',fontStyle:'italic'}}>لذت غذای سالم و گیاهی را با ترخینه تجربه کنید </h4>
+               
+            </div>
+            
+          </Carousel>
+       
+     );
+}
+ 
+export default Slider;
