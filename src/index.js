ebbd04edf0cc2f583623080dbe3ed94c62631fdb
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-08-13 13:23:16 +0330

    import font.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './styles/input.css';
+import './styles/font.css';
 import {BrowserRouter} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
