3857b866afea1a9f440a3a9f1d685dc631a2909c
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-08-14 21:43:01 +0330

    Update Foods.js

--- src/pages/shop/components/Foods.js
+++ src/pages/shop/components/Foods.js
@@ -1,10 +1,10 @@
-import { Button} from 'antd';
-import { FaStar} from 'react-icons/fa';
+import { Badge, Button} from 'antd';
+import { FaStar,HeartOutlined} from 'react-icons/fa';
 
-const Foods = ({title,foodImage,starCount,scoreCount,discountedPercent,hasDiscount,discountedPrice,price}) => {
+const Foods = ({title,foodImage,starCount,scoreCount,hasDiscount,discountedPercent,discountedPrice,price}) => {
     return ( 
         
-                 <div className=' flex-col justify-center  items-center m-5 border border-gray-400 rounded-md shadow-xl hover:-translate-y-2 transition-all' style={{boxShadow:"0.5rem 0.75rem 1.5rem #bbbbbb",background:'var --color-gray-primary'}}>
+                 <div className=' flex-col justify-center  items-center m-5 border border-gray-400 rounded-md shadow-xl' style={{boxShadow:"0.5rem 0.75rem 1.5rem #bbbbbb",background:'var --color-gray-primary'}}>
                     <img src={foodImage} className='productImage' alt='foodimage'/>
                     <div className='flex flex-col gap-2 justify-between items-center'>
                     <h3 className='pt-3 text-xl'>{title}</h3>
@@ -14,7 +14,7 @@ const Foods = ({title,foodImage,starCount,scoreCount,discountedPercent,hasDiscou
                     <div className='flex flex-row-reverse gap-3 mt-5 '>
                         <div className='flex flex-col  justify-end items-center '>
                             <div className='text-xs pb-2' style={{background:"url(./images/heart)"}} >
-                            {/* <HeartOutlined/>  */}
+                           {/* <HeartOutlined/>   */}
                                 افزودن به علاقه مندی ها</div>
                             <div className='flex flex-row-reverse justify-end items-right '>
                                     <FaStar style={{fill:'orange', paddingLeft:'2px'}}>{starCount}</FaStar>
@@ -27,9 +27,10 @@ const Foods = ({title,foodImage,starCount,scoreCount,discountedPercent,hasDiscou
                             </div>
                         </div>  
                     <div className='flex flex-col justify-start items-center gap-1'>
+
                         <div style={{visibility:hasDiscount?'visible':'hidden'}}   className='flex flex-row justify-start items-center'>
-                        <span className=' rounded-xl flex justify-center  items-center bg-red-300 w-7 h-5 text-sm'>%{discountedPercent}
-                        </span>
+                        <Badge count={`${discountedPercent}%`} />
+
                         <span className=' line-through text-gray-400 text-sm pl-1'>{discountedPrice}</span> 
                         </div>
                        
@@ -38,7 +39,9 @@ const Foods = ({title,foodImage,starCount,scoreCount,discountedPercent,hasDiscou
                    
                     </div>
                     </div>
-                    <Button style={{background:'green',lineHeight:'20px' ,textAlign:'center', color:"white", margin:'15px' , width:"256px"}} >افزودن به سبد خرید </Button>
+                    <Button style={{background:'green',lineHeight:'20px' ,textAlign:'center', color:"white", margin:'15px' , width:"256px"}}>
+                        افزودن به سبد خرید
+                     </Button>
                     </div>
       
      );
