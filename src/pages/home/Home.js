e8945ee03e5609ec57e102abea9f8a11f190a658
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-08-24 11:35:46 +0330

    import TopSlider in shop and home pages

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,5 @@
 
-
+import TopSlider from './component/slider/TopSlider';
 import Branches from '../home/homeComponents/Branches';
 import Menu from '../home/homeComponents/Menu';
 import About from '../home/homeComponents/About';
@@ -7,7 +7,7 @@ import About from '../home/homeComponents/About';
 const Home =()=>{
 return(
     <>
-      
+       <div><TopSlider/></div>
       <div><Menu/></div>
       <div><About/></div>
       <div><Branches/></div> 
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,8 +1,10 @@
+import TopSlider from './component/slider/TopSlider';
 import ShopSlider from "./components/ShopSlider";
 
 const Shop = () => {
    return ( 
       <>
+      <div><TopSlider/></div>
      <div> <ShopSlider/></div>
       
     <div className="bg-[#315F41]"> <ShopSlider/></div>
