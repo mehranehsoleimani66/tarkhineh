6d5244f0c929b62176c141c20dd8c17853089140
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-08-24 10:52:34 +0330

    create usecontext and using localstorage to set products

--- src/App.js
+++ src/App.js
@@ -6,23 +6,50 @@ import Slider from './component/slider/Slider';
 import Home from './pages/home/Home';
 import {Route, Routes} from 'react-router-dom';
 import Shop from './pages/shop/Shop';
+import { createContext,useEffect,useState } from 'react';
 
 
 
+export const cartContext = createContext()
 function App() {
+  const [cardFood,setCardFood]=useState([])
+  // check the  localstorage at the first render and  set the first food
+  useEffect(()=>{
+    const prevCartArr = JSON.parse( localStorage.getItem('cart'))
+    if (prevCartArr){
+      setCardFood(prevCartArr)
+    }
+    else{
+      setCardFood([])
+    }
+   }
+   ,[])
+   // set the second or more foods
+   useEffect(()=>{
+     if (cardFood!==undefined && cardFood.length>0){
+      localStorage.setItem('cart',JSON.stringify(cardFood))
+     }
+   },[cardFood])
+ 
+    const buyFoodHandler=(item)=>{
+    setCardFood(item)
+
+   }
+
   return (
     <div className="App">
 <React.StrictMode>
+  <cartContext.Provider value={{cardFood,buyFoodHandler}}>
       <Header/>
       <Slider/>
       <Routes>
-      
      <Route index element={<Home/>} />
-     <Route path='/Shop' element={<Shop/>}/>
+     <Route path='/Shop' element={<Shop />}/>
      
     
       </Routes>
       <Footer/>
+  </cartContext.Provider>
       </React.StrictMode>
       
     </div>
