91c3ea8618f63a1a9a0bab7efebf0b6e9ba9b63b
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-09-09 12:45:59 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 import Footer from './component/Footer/Footer';
 import Header from './component/header/Header';
 //import Slider from './component/slider/Slider';
@@ -9,13 +9,44 @@ import Shop from './pages/shop/Shop';
 import { createContext,useEffect,useState } from 'react';
 import CardPage from './pages/cart/CardPage';
 import CardList from './pages/cart/CardList';
-import Profile from './pages/Profile';
-
-
+import Login from './pages/login/Login';
+//import Profile from './pages/Profile';
 
 export const cartContext = createContext()
+export const userContext = createContext()
 function App() {
+
   const [cardFood,setCardFood]=useState([])
+  const [user,setUser]=useState({})
+
+  //test
+
+  useEffect(()=>{
+  console.log (user,'user dideh shod')
+},[])
+
+useEffect(()=>{
+  
+  if (user?.token){
+   localStorage.setItem('user',JSON.stringify(user))
+  }
+},[user])
+
+
+
+
+useEffect(()=>{
+  const prevUser = JSON.parse( localStorage.getItem('user'))
+  if (prevUser){
+    setUser(prevUser)
+  }
+  else {
+    setUser([])
+  }
+  
+}
+ ,[])
+
   // check the  localstorage at the first render and  set the first food
   useEffect(()=>{
     const prevCartArr = JSON.parse( localStorage.getItem('cart'))
@@ -28,6 +59,7 @@ function App() {
     
   }
    ,[])
+
    // set the second or more foods
    useEffect(()=>{
      if (cardFood!==undefined){
@@ -39,21 +71,29 @@ function App() {
     setCardFood(item)
 
    }
+   const setUserHandler=(item)=>{
+    setUser(item)
+    
+
+   }
 
   return (
     <div className="App">
 <React.StrictMode>
   <cartContext.Provider value={{cardFood,buyFoodHandler}}>
+  <userContext.Provider value={{user,setUserHandler}}>
       <Header/>
       
       <Routes>
      <Route index element={<Home/>} />
      <Route path='/Shop' element={<Shop />}/>
-     <Route path='/Card' element={<CardPage />}/>
+     <Route path='/CardPage' element={<CardPage />}/>
      <Route path='/Cardlist' element={<CardList />}/>
-     <Route path='/profile' element={<Profile />}/>
+     <Route path='/login' element={<Login />}/>
+     {/* <Route path='/profile' element={<Profile />}/> */}
       </Routes>
       <Footer/>
+  </userContext.Provider>
   </cartContext.Provider>
       </React.StrictMode>
       
