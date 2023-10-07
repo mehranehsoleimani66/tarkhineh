952a22b2bf66cfa6567501203c7e6080cee7cc31
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-07 23:59:59 +0330

    Update header.js

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,29 +1,14 @@
-import { useEffect, useState } from "react"
-import Icons from "./Icons"
-import Logo from "./Logo"
-import Navbar from "./Navbar"
+import { useEffect, useState } from "react";
+import Icons from "./Icons";
+import Logo from "./Logo";
+import Navbar from "./Navbar";
 
+const Header = () => {
+  return (
+    <div className=" w-full flex flex-row justify-between items-center border-b-2 shadow-slate-300  bg-gray-primary h-24">
+      <Navbar />
+    </div>
+  );
+};
 
-const Header=()=>{
- 
-
-    return(
-            <div className=" w-full flex flex-row justify-between items-center border-b-2 shadow-slate-300  bg-gray-primary h-24">
-            
-<Navbar/>
-{/*            
-
-            <div className="" >
-              <Navbar/>
-            </div>
-
-             <div className=" w-1/2 items-center md:w-20 mx-20 my-2 ">
-                <Logo/>
-            </div> 
- 
-           <div className="md:hidden  mr-5" ><img className="w-28 h-10" src='./images/menu.png'/></div> */}
-            </div>
-    )
-}
-
-export default Header
+export default Header;
